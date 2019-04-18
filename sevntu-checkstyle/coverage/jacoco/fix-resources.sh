e5433308643ef1b85a86f72e7eb8d33f74860ed5
Martin Dietze <mdietze@gmail.com>
2019-04-18 17:00:34 +0200

    sevntu-checkstyle: repaired resources again

--- sevntu-checkstyle/coverage/jacoco/fix-resources.sh
+++ sevntu-checkstyle/coverage/jacoco/fix-resources.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-perl -pi -e 's/\.resources/resources/g' `find . -name '*.html' | XargsGrep -l '\.resources'`
+perl -pi -e 's/\.resources/resources/g' `find . -name '*.html' | xargs grep -l '\.resources'`
